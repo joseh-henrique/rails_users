0f03b8f77d5d4756c1e72b150d4e8f27ded6b8a5
Prelang Builder <builder@prelang.com>
2015-11-05 18:16:59 +0000

    Generated model Member with migration

---
+++ db/migrate/20151105181659_create_members.rb
@@ -0,0 +1,10 @@
+class CreateMembers < ActiveRecord::Migration
+  def change
+    create_table :members do |t|
+      t.string :name
+      t.references :congregation, index: true
+
+      t.timestamps
+    end
+  end
+end
